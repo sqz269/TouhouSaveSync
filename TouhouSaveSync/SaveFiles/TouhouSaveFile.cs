753a4a6baa3314b2f16243f06366abd924de89f6
sqz269 <yin2690855381@gmail.com>
2020-10-17 16:59:06 -0500

    Added few utility functions for syncing

--- TouhouSaveSync/SaveFiles/TouhouSaveFile.cs
+++ TouhouSaveSync/SaveFiles/TouhouSaveFile.cs
@@ -43,6 +43,45 @@ namespace TouhouSaveSync.SaveFiles
         }
 
         /// <summary>
+        /// Get the save file's creation time in Unix timestamp
+        /// </summary>
+        /// <returns>A double representing seconds after 1970/1/1</returns>
+        public double GetZipSaveFileModifyTime()
+        {
+            DateTime dateTime = File.GetLastWriteTime(this.ZipSaveStoragePath);
+            return dateTime.Subtract(DateTime.UnixEpoch).TotalSeconds;
+        }
+
+        /// <summary>
+        /// Get the name of the save file that should be stored as in google drive
+        /// </summary>
+        /// <returns>The name to store the save file as (Same as this.GameTitle)</returns>
+        public string GetRemoteFileName()
+        {
+            return this.GameTitle;
+        }
+
+        /// <summary>
+        /// Get the last mod date of the score.dat file in unix time
+        /// <br></br>
+        /// This is an important metric to determine if we want to upload the save file or download the save file
+        /// </summary>
+        /// <returns>A double representing seconds after 1970/1/1</returns>
+        public double GetScoreDatModifyTime()
+        {
+            foreach (string f in Directory.GetFiles(this.GameSavePath, "*.dat"))
+            {
+                if (f.StartsWith("score"))
+                {
+                    DateTime dateTime = File.GetLastWriteTime(f);
+                    return dateTime.Subtract(DateTime.UnixEpoch).TotalSeconds;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
         /// Zip the folder at GameSaveFile and store it at ZipSaveStoragePath
         /// Then, return the checksum of the zip file
         /// <br></br>
