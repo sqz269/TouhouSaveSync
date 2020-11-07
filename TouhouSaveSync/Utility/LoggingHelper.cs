52e2b450c6e05a4d0777dd88ce3ebc9eeeae4a28
sqz269 <yin2690855381@gmail.com>
2020-11-07 13:29:09 -0600

    Added file to logging

--- TouhouSaveSync/Utility/LoggingHelper.cs
+++ TouhouSaveSync/Utility/LoggingHelper.cs
@@ -9,11 +9,22 @@ namespace TouhouSaveSync.Utility
         {
             var config = new NLog.Config.LoggingConfiguration();
 
-            var logConsole = new NLog.Targets.ColoredConsoleTarget("logconsole");
-            logConsole.Layout = Layout.FromString("${time} | <${logger:shortName=true}> [${level:uppercase=true}]: ${message} ${exception:format=tostring}");
+            var logConsole = new NLog.Targets.ColoredConsoleTarget("logconsole")
+            {
+                Layout = Layout.FromString(
+                    "${time} | <${logger:shortName=true}> [${level:uppercase=true}]: ${message} ${exception:format=tostring}")
+            };
+
+            var logFile = new NLog.Targets.FileTarget("logfile")
+            {
+                FileName = "log.log",
+                Layout = Layout.FromString(
+                    "${time} | <${logger}> [${level:uppercase=true}]: ${message} ${exception:format=tostring}")
+            };
 
             config.AddRule(LogLevel.Trace, LogLevel.Fatal, logConsole);
-            
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logFile);
+
             NLog.LogManager.Configuration = config;
         }
     }
